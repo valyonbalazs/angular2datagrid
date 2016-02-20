dc64e466808868496076c06d17e70ca64d3e304a
Valyon Balázs <valyon.balazs@gmail.com>
2016-02-20 21:31:59 +0100

    initial commit

---
+++ AspNet5Angular2/gulpfile.js
@@ -0,0 +1,25 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+/// <binding AfterBuild='moveToLibs' />
+
+var gulp = require('gulp');
+
+var paths = {
+    npmSrc: "./node_modules/",
+    libTarget: "./wwwroot/libs/"
+};
+
+var libsToMove = [
+   paths.npmSrc + '/angular2/bundles/angular2-polyfills.js',
+   paths.npmSrc + '/systemjs/dist/system.js',
+   paths.npmSrc + '/systemjs/dist/system-polyfills.js',
+   paths.npmSrc + '/rxjs/bundles/Rx.js',
+   paths.npmSrc + '/angular2/bundles/angular2.dev.js',
+   paths.npmSrc + '/es6-shim/es6-shim.min.js'
+];
+gulp.task('moveToLibs', function () {
+    return gulp.src(libsToMove).pipe(gulp.dest(paths.libTarget));
+});
