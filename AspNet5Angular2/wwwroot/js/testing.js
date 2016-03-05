621190b480c7f9c64f78a9632a291054ddd86d1a
Valyon Balázs <valyon.balazs@gmail.com>
2016-03-05 16:32:13 +0100

    directory restructuring

--- AspNet5Angular2/wwwroot/js/testing.js
+++
@@ -1,9 +0,0 @@
-var Testing = (function () {
-    function Testing() {
-    }
-    Testing.prototype.Method1 = function () {
-        return 1;
-    };
-    return Testing;
-})();
-//# sourceMappingURL=testing.js.map
---
+++ AspNet5Angular2/wwwroot/test/test-datacontainer2.ts
@@ -0,0 +1,20 @@
+﻿/// <reference path="./typings/mocha.d.ts" />
+/// <reference path="./typings/qunit.d.ts" />
+
+import dc = require('../scripts/data-container');
+QUnit.module('../scripts/data-container');
+test("basic", function () {
+
+    var a = 1;      
+    
+    var DataContainer = dc.DataContainer;
+    var rows = [
+        { firstName: 'Joe', lastName: 'Jackson', age: 1 },
+        { firstName: 'Peter', lastName: 'Smith', age: 2 }];
+
+    DataContainer.setRows(rows);
+    equal(DataContainer.getNumberOfRows(), 2);
+
+
+});
+
