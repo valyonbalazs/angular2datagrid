78acf052fe1ccd4b3cc562f08d6d683d0a169f8d
Valyon Balázs <valyon.balazs@gmail.com>
2016-03-05 13:43:10 +0100

    test file sees the module, but require not loaded problem still

---
+++ AspNet5Angular2/Tests_ts/tests/test-datacontainer2.ts
@@ -0,0 +1,21 @@
+﻿/// <reference path="../typings/mocha.d.ts" />
+/// <reference path="../typings/chai.d.ts" />
+/// <reference path="../typings/qunit.d.ts" />
+
+// import * as DataContainer from '../../wwwroot/scripts/data-container';
+// import {DataContainer} from '../../wwwroot/scripts/data-container';
+
+import dc = require('../../wwwroot/scripts/data-container');
+QUnit.module('../../wwwroot/scripts/data-container');
+test("basic", function () {
+
+    var DataContainer = dc.DataContainer;
+    var rows = [
+        { firstName: 'Joe', lastName: 'Jackson', age: 1 },
+        { firstName: 'Peter', lastName: 'Smith', age: 2 }];
+
+    DataContainer.setRows(rows);
+    equal(DataContainer.getNumberOfRows(), 2);
+
+});
+
---
+++ AspNet5Angular2/wwwroot/js/testing.js
@@ -0,0 +1,9 @@
+var Testing = (function () {
+    function Testing() {
+    }
+    Testing.prototype.Method1 = function () {
+        return 1;
+    };
+    return Testing;
+})();
+//# sourceMappingURL=testing.js.map
